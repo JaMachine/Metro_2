f55485d2cc04cc9164f9e0abc4867b0d8fd2c8db
JaMachine <fenia5500@meta.ua>
2017-05-26 16:39:49 +0300

    v2

--- src/metro/Train.java
+++ src/metro/Train.java
@@ -1,9 +1,31 @@
 package metro;
 
+import java.util.ArrayList;
+
 public class Train {
-	
+
 	private static int trainId = 0;
-	
-	
+	private int id;
+
+	ArrayList<Wagon> train = new ArrayList<>();
+
+	public Train() {
+		trainId++;
+		id = trainId;
+
+		train.add(new Wagon(true));
+		train.add(new Wagon(true));
+		for (int i = 0; i < 3; i++)
+			train.add(new Wagon(false));
+	}
+
+	public ArrayList<Wagon> getTrain() {
+		return train;
+	}
+
+	public int getId() {
+		return id;
+
+	}
 
 }
--- src/metro/aaMain.java
+++ src/metro/aaMain.java
@@ -8,6 +8,22 @@ public class aaMain {
 
 		//
 
+		//
+
+		// Train t = new Train();
+		//
+		// System.out.println(t.getTrain().get(1).getC());
+		// for (int i = 0; i < 123; i++) {
+		// t.getTrain().get(1).capacity.add(new Passenger());
+		//
+		// }
+		// System.out.println((t.getTrain().get(1).getC()) -
+		// (t.getTrain().get(1).capacity.size()));
+
+		//
+
+		//
+
 		// Wagon w = new Wagon(true);
 		//
 		// while (w.capacity.size() < w.getC)
